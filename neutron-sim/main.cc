69207bf459c85fd26f559ee45c0bec660f34ca8d
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:45:02 +0300

    Update main.cc

--- neutron-sim/main.cc
+++ neutron-sim/main.cc
@@ -1,1 +1,26 @@
+#include "G4RunManagerFactory.hh"
+#include "G4UImanager.hh"
+#include "G4PhysListFactory.hh"
+#include "G4SystemOfUnits.hh"
 
+#include "ActionInitialization.hh"
+#include "DetectorConstruction.hh"
+
+int main() {
+    auto runManager = G4RunManagerFactory::CreateRunManager();
+
+    runManager->SetUserInitialization(new DetectorConstruction());
+
+    G4PhysListFactory factory;
+    runManager->SetUserInitialization(factory.GetReferencePhysList("FTFP_BERT"));
+
+    runManager->SetUserInitialization(new ActionInitialization());
+
+    runManager->Initialize();
+
+    G4UImanager::GetUIpointer()->ApplyCommand("/run/initialize");
+    G4UImanager::GetUIpointer()->ApplyCommand("/control/execute run.mac");
+
+    delete runManager;
+    return 0;
+}
