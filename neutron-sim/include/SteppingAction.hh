3f7d9a4052433adb6ec1d54d6f61fb5729356ee6
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:48:15 +0300

    Update SteppingAction.hh

--- neutron-sim/include/SteppingAction.hh
+++ neutron-sim/include/SteppingAction.hh
@@ -1,1 +1,17 @@
+#ifndef STEPPING_ACTION_H
+#define STEPPING_ACTION_H
 
+#include "G4UserSteppingAction.hh"
+#include "EventAction.hh"
+
+class SteppingAction : public G4UserSteppingAction {
+public:
+    SteppingAction(EventAction*);
+    virtual ~SteppingAction() = default;
+    virtual void UserSteppingAction(const G4Step*) override;
+
+private:
+    EventAction* fEventAction;
+};
+
+#endif
