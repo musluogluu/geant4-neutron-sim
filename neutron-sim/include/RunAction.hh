5f3ecebbc3369bbb94130f069c0d67cfe67067c3
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:47:23 +0300

    Update RunAction.hh

--- neutron-sim/include/RunAction.hh
+++ neutron-sim/include/RunAction.hh
@@ -1,1 +1,14 @@
+#ifndef RUN_ACTION_H
+#define RUN_ACTION_H
 
+#include "G4UserRunAction.hh"
+
+class RunAction : public G4UserRunAction {
+public:
+    RunAction() = default;
+    virtual ~RunAction() = default;
+    virtual void BeginOfRunAction(const G4Run*) override;
+    virtual void EndOfRunAction(const G4Run*) override;
+};
+
+#endif
