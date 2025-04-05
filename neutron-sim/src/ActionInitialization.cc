dff6e2c80defc2a13a022a7c8467e92da8611948
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:47:10 +0300

    Update ActionInitialization.cc

--- neutron-sim/src/ActionInitialization.cc
+++ neutron-sim/src/ActionInitialization.cc
@@ -1,1 +1,13 @@
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "EventAction.hh"
+#include "SteppingAction.hh"
 
+void ActionInitialization::Build() const {
+    auto eventAction = new EventAction();
+    SetUserAction(new PrimaryGeneratorAction());
+    SetUserAction(new RunAction());
+    SetUserAction(eventAction);
+    SetUserAction(new SteppingAction(eventAction));
+}
