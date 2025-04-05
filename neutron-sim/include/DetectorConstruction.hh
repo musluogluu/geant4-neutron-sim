4fb770965f1727d3331060d36761fb3feb3b8744
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:45:41 +0300

    Update DetectorConstruction.hh

--- neutron-sim/include/DetectorConstruction.hh
+++ neutron-sim/include/DetectorConstruction.hh
@@ -1,1 +1,13 @@
+#ifndef DETECTOR_CONSTRUCTION_H
+#define DETECTOR_CONSTRUCTION_H
 
+#include "G4VUserDetectorConstruction.hh"
+
+class DetectorConstruction : public G4VUserDetectorConstruction {
+public:
+    DetectorConstruction() = default;
+    virtual ~DetectorConstruction() = default;
+    virtual G4VPhysicalVolume* Construct();
+};
+
+#endif
