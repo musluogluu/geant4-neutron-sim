caf94af6b44d7179a566eeaf34819e76521ad498
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:48:04 +0300

    Update EventAction.hh

--- neutron-sim/include/EventAction.hh
+++ neutron-sim/include/EventAction.hh
@@ -1,1 +1,14 @@
+#ifndef EVENT_ACTION_H
+#define EVENT_ACTION_H
 
+#include "G4UserEventAction.hh"
+
+class EventAction : public G4UserEventAction {
+public:
+    EventAction() = default;
+    virtual ~EventAction() = default;
+    virtual void BeginOfEventAction(const G4Event*) override {}
+    virtual void EndOfEventAction(const G4Event*) override {}
+};
+
+#endif
