b311a0098cd51da82c0d09a8bb2de3fa19f45264
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:47:47 +0300

    Update RunAction.cc

--- neutron-sim/src/RunAction.cc
+++ neutron-sim/src/RunAction.cc
@@ -1,1 +1,16 @@
+#include "RunAction.hh"
+#include "G4AnalysisManager.hh"
 
+void RunAction::BeginOfRunAction(const G4Run*) {
+    auto analysisManager = G4AnalysisManager::Instance();
+    analysisManager->OpenFile("output.root");
+
+    analysisManager->CreateH1("neutron_angle", "Neutron Angle [deg]", 180, 0, 180);
+    analysisManager->CreateH1("neutron_energy", "Neutron Energy [MeV]", 100, 0, 1000);
+}
+
+void RunAction::EndOfRunAction(const G4Run*) {
+    auto analysisManager = G4AnalysisManager::Instance();
+    analysisManager->Write();
+    analysisManager->CloseFile();
+}
