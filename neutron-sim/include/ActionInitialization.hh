283f05679b48558f7b50d262aa1b4f09e0e80c34
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:46:54 +0300

    Update ActionInitialization.hh

--- neutron-sim/include/ActionInitialization.hh
+++ neutron-sim/include/ActionInitialization.hh
@@ -1,1 +1,13 @@
+#ifndef ACTION_INITIALIZATION_H
+#define ACTION_INITIALIZATION_H
 
+#include "G4VUserActionInitialization.hh"
+
+class ActionInitialization : public G4VUserActionInitialization {
+public:
+    ActionInitialization() = default;
+    virtual ~ActionInitialization() = default;
+    virtual void Build() const override;
+};
+
+#endif
