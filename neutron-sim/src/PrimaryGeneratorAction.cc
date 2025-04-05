11f45ac4f5e601de96dda4b1b802edf9d7f87beb
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:46:41 +0300

    Update PrimaryGeneratorAction.cc

--- neutron-sim/src/PrimaryGeneratorAction.cc
+++ neutron-sim/src/PrimaryGeneratorAction.cc
@@ -1,1 +1,23 @@
+#include "PrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "G4SystemOfUnits.hh"
 
+PrimaryGeneratorAction::PrimaryGeneratorAction() {
+    fParticleGun = new G4ParticleGun(1);
+    auto particleTable = G4ParticleTable::GetParticleTable();
+    auto proton = particleTable->FindParticle("proton");
+
+    fParticleGun->SetParticleDefinition(proton);
+    fParticleGun->SetParticleEnergy(1 * GeV);
+    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
+    fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -40 * cm));
+}
+
+PrimaryGeneratorAction::~PrimaryGeneratorAction() {
+    delete fParticleGun;
+}
+
+void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event) {
+    fParticleGun->GeneratePrimaryVertex(event);
+}
