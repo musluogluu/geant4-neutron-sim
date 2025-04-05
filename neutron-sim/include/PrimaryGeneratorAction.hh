65ef13ac349375295eb259ef9b2c7cd4d1d0f147
muslu <musluoglu.mert10@gmail.com>
2025-04-05 16:46:29 +0300

    Update PrimaryGeneratorAction.hh

--- neutron-sim/include/PrimaryGeneratorAction.hh
+++ neutron-sim/include/PrimaryGeneratorAction.hh
@@ -1,1 +1,16 @@
+#ifndef PRIMARY_GENERATOR_ACTION_H
+#define PRIMARY_GENERATOR_ACTION_H
 
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
+public:
+    PrimaryGeneratorAction();
+    virtual ~PrimaryGeneratorAction();
+    virtual void GeneratePrimaries(G4Event*);
+private:
+    G4ParticleGun* fParticleGun;
+};
+
+#endif
